8107c1f219e6b1c7012dcf2fd55603e93c836cc2
Dominic Tarr <dominic.tarr@gmail.com>
2013-03-26 12:54:21 +0000

    allow running on an exact port, and disableing app

--- bin/cmd.js
+++ bin/cmd.js
@@ -4,6 +4,7 @@ var shoe = require('shoe');
 var http = require('http');
 var shux = require('shux')();
 var spawn = require('child_process').spawn;
+var opts  = require('optimist').argv;
 
 var ecstatic = require('ecstatic')(__dirname + '/../static');
 var server = http.createServer(function (req, res) {
@@ -12,7 +13,7 @@ var server = http.createServer(function (req, res) {
     }
     else ecstatic(req, res)
 });
-server.listen(0, '127.0.0.1');
+server.listen(opts.port || 0, '127.0.0.1');
 
 var sock = shoe(function (stream) {
     stream.pipe(shux.createShell()).pipe(stream);
@@ -21,5 +22,7 @@ sock.install(server, '/sock');
 
 server.on('listening', function () {
     var port = server.address().port;
-    spawn('google-chrome', [ '--app=http://localhost:' + port ]);
+    if(opts.app !== false)
+        spawn('google-chrome', [ '--app=http://localhost:' + port ]);
 });
+
